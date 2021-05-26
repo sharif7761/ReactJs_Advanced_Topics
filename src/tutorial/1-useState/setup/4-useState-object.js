6f29d83b6eb13e788372797a82f7949f91e5e8f8
sharif7761 <sharif7761@gmail.com>
2021-05-27 02:04:48 +0600

    useState with multiple values

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -7,8 +7,14 @@ const UseStateObject = () => {
     message: "software engineer",
   })
 
+  // or use multiple useState for each of them
+  const [name, setName] = useState("Ahmed")
+  const [age, setAge] = useState(23)
+  const [message, setMessage] = useState("React Developer")
+
   const changeMessage = () => {
     setPerson({...person, message: 'Dreamer'})
+    setMessage('Laravel Developer')
   }
 
   return (
@@ -16,6 +22,9 @@ const UseStateObject = () => {
         <h3>{person.name}</h3>
         <h3>{person.age}</h3>
         <h3>{person.message}</h3>
+        <h3>{name}</h3>
+        <h3>{age}</h3>
+        <h3>{message}</h3>
         <button className='btn' onClick={changeMessage}>Change message</button>
       </>
   )

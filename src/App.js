db1e0a1772a7810e0afc5a02830b114562a5fd0f
sharif7761 <sharif7761@gmail.com>
2021-05-26 22:11:25 +0600

    useState with array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/1-error-example'
+import Basic from './tutorial/1-useState/setup/1-error-example'
+import Array from './tutorial/1-useState/setup/3-useState-array'
 function App() {
   return (
     <div className='container'>
-      <Setup />
+        <h1>useState Hook</h1>
+        <hr/><br/>
+        <Basic />
+        <hr/><br/>
+        <Array />
     </div>
   )
 }
---
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { data } from '../../../data';
+
+const UseStateArray = () => {
+  const [people, setPeople] = useState(data)
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id )
+    setPeople(newPeople)
+  }
+  return (
+      <>
+        <h2>Use State with array</h2>
+        {
+          people.map((person) => {
+            const {id, name} = person
+            return (
+                <div key={id} className='item'>
+                  <h4>{name}</h4>
+                  <button onClick={() => removeItem(id)}>remove</button>
+                </div>
+            )
+          })
+        }
+        <button className='btn' onClick={() => setPeople([])}>
+          Clear Items
+        </button>
+      </>
+  )
+};
+
+export default UseStateArray;

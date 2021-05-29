84866c67cf07b5782f5c8c5073ce59c24501bb7f
sharif7761 <sharif7761@gmail.com>
2021-05-30 01:11:01 +0600

    fetch data and multiple return

--- src/tutorial/3-conditional-rendering/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/1-multiple-returns.js
@@ -2,16 +2,34 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
   const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+  const [user, setUser] = useState('default user')
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false)
+  //   },5000)
+  // },[loading])
+
   useEffect(() => {
-    setTimeout(() => {
-      setLoading(false)
-    },5000)
-  },[loading])
+    fetch(url)
+        .then((resp) => resp.json())
+        .then((user) => {
+          const {login} = user
+          setUser(login)
+          setLoading(false)
+        })
+        .catch((error) => console.log(error))
+
+  },[])
 
   if (loading) {
     return <h2>Loading...</h2>
   }
-  return <h2>multiple returns</h2>;
+  if (error) {
+    return <h2>Something is wrong...</h2>
+  }
+  return <h2>{user}</h2>;
 };
 
 export default MultipleReturns;
